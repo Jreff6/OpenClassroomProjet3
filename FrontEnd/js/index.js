642bea03935c69159443ea4b59d16d76bd35beb8
Jreff6 <enzo.solaar@hotmail.fr>
2024-03-26 17:50:48 +0100

    modification on the main function

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,8 +1,49 @@
-function init(){
+let projets = [];
+let categories = [];
+
+
+async function init(){
     console.log("Starting");
   
-    fetch('http://localhost:5678/api/works')
-    .then(response =>(response.json()))
-    .then(data => sendData(data));
+    await fetch('http://localhost:5678/api/works')
+    .then((response) =>(response.json()))
+    .then((data) => (projets = data));
 };
 init();
+
+
+
+function genererProjet(donneesProjets){
+      console.log(donneesProjets);
+      document.querySelector(".gallery").innerHTML = "";
+      for (let i = 0; i < donneesProjets.length; i++) {
+          {
+                  
+              const figure = donneesProjets[i];
+              
+              const sectionFiches = document.querySelector(".gallery");
+              const pieceElement = document.createElement("figure"); 
+              const imageElement = document.createElement("img");
+              imageElement.src = figure.imageUrl;
+              const nomElement = document.createElement("figcaption");
+              nomElement.innerText = figure.title;
+              
+            
+              sectionFiches.appendChild(pieceElement);
+              pieceElement.appendChild(imageElement);
+              pieceElement.appendChild(nomElement);
+                     
+          }
+      }
+};
+
+window.addEventListener("load", ()=>{
+  genererProjet(projets)
+})
+
+async function GetCategory(){
+
+  await fetch('http://localhost:5678/api/categories')
+  .then((response) =>(response.json()))
+  .then((data) => (categories = data))}
+

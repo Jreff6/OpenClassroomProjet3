346040049f9f0106d605ef009a8545ac94e08fb8
Jreff6 <enzo.solaar@hotmail.fr>
2024-04-04 20:20:04 +0200

    started login logic

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,28 +1,22 @@
-// Récupérer les éléments nécessaires depuis le DOM
-const modal = document.getElementById('loginModal');
-const btnLogin = document.getElementById('btnLogin');
-const btnClose = document.getElementsByClassName('close')[0];
-
-// Fonction pour ouvrir la modale
-function openModal() {
-    modal.style.display = 'flex';
-  }
-  
-// Fonction pour fermer la modale
-function closeModal() {
-    modal.style.display = 'none';
-  }
-  
-// Ouvrir la modale lorsqu'on clique sur le bouton Login
-btnLogin.addEventListener('click', openModal);
-  
-// Fermer la modale lorsqu'on clique sur le bouton de fermeture (×)
-btnClose.addEventListener('click', closeModal);
 
 
-// Envoyer les données de connexion au backend [NOT FINISHED]
-const loginForm = document.getElementById('login-form');
 
 loginForm.addEventListener('submit', (event) => {
-  console.log('Formulaire soumis !');
+  event.preventDefault();
+  let userInfo = {
+    "email" : email.value,
+    "password" : password.value
+  }
+
+  console.log(userInfo);
 });
+
+
+
+
+
+
+//function checkInput({}})
+
+
+//localStorage.setItem("token", response.token)

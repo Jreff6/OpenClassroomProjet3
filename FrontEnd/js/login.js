6fd8bfe38b35e3d73b0531d0429c2f6b4d6494d7
Jreff6 <enzo.solaar@hotmail.fr>
2024-04-09 19:26:33 +0200

    started the login logic

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -7,16 +7,32 @@ loginForm.addEventListener('submit', (event) => {
     "email" : email.value,
     "password" : password.value
   }
+connect();
 
   console.log(userInfo);
 });
 
+async function connect (userInfo) {
+
+  const apiURL ='http://localhost:5678/api/users/login'
+  const options = {
+    method : 'post',
+    headers : {
+      'content-Type' : 'application/json'
+    },
+    body: JSON.stringify(userInfo)
+  };
+  fetch(apiURL, options)
+    .then( response => 
+      {
+        if (!response.ok){
+          throw new Error('Erreur lors de la requête');
+        }
+      return response.json  
+      })
+console.log(reponse.token)
+}
 
 
-
-
-
+//localStorage.setItem("token", response.token);
 //function checkInput({}})
-
-
-//localStorage.setItem("token", response.token)
